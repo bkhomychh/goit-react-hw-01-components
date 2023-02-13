084f964c463de1a34918ef2505b6e1d5a358eb82
Bohdan Khomych <bod.khomych@gmail.com>
2023-02-13 15:23:38 +0200

    remove needless code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
