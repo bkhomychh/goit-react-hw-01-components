fb49289ceba9bf1ab09efe016ba1d8fea7dff69c
Bohdan Khomych <bod.khomych@gmail.com>
2023-03-01 18:04:44 +0200

    correct mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
-import user from 'user.json';
-import data from 'data.json';
-import friends from 'friends.json';
-import transactions from 'transactions.json';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 const { username, tag, location, avatar, stats } = user;
 
